8f50de46024f645b40898a618497523bc9722e8c
Anatolii <anatoliy627801@gmail.com>
2023-02-27 18:51:01 +0200

    add blog

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,5 @@
 import Header from './../../../alpina-ui/src/components/Header/Header'
-
+import Blog from '../../../alpina-ui/src/components/Blog/Blog'
 // import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import Navbar from '../Nav/Navbar';
